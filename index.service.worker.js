8591fd4f4f2fc2ca57d77c2691870b97b96bba57
aforry <aforry1@gmail.com>
2026-01-24 00:28:19 +0000

    Deploying to gh-pages from @ Technical-Incompetence/melty-balls@44b9565171dca74b5a6bc8258ebd305f89946ccd 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769144707|5448836';
+const CACHE_VERSION = '1769214480|9333787';
 /** @type {string} */
 const CACHE_PREFIX = 'Melty Balls-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
