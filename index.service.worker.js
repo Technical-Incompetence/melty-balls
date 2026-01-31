b1453c6f8ba8254daca3265d0bb730a89a235f13
numberQ <numberQ+github@proton.me>
2026-01-31 19:09:37 +0000

    Deploying to gh-pages from @ Technical-Incompetence/melty-balls@e37c819bf127039e14102a654a6d89617717427c 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769576469|8975316';
+const CACHE_VERSION = '1769886555|10184103';
 /** @type {string} */
 const CACHE_PREFIX = 'Melty Balls-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
