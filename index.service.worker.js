f52f7076190cc2d624b8ba425c52dc2c6fd88c11
flamwenco <danielmuckerman@me.com>
2026-02-05 03:40:56 +0000

    Deploying to gh-pages from @ Technical-Incompetence/melty-balls@c573116198be7ab49d92ef8f97f30426450d2588 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769903057|11688067';
+const CACHE_VERSION = '1770262832|10579832';
 /** @type {string} */
 const CACHE_PREFIX = 'Melty Balls-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
